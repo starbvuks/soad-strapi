a896cdf69a9303ca9bfe755519896af2f1ed9dde
starbvuks <starbvuks@gmail.com>
2024-03-06 16:22:49 +0530

    s3

---
+++ src/api/academic-year/services/academic-year.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * academic-year service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::academic-year.academic-year');
